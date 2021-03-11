f4247b773559618f5efa7302aea0e9319904e4e5
Zury <62314188+VL0511@users.noreply.github.com>
2021-03-11 02:38:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login and Registration System</title>
     <link rel="stylesheet" href="./view/bootstrap/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <center>
-                <a href="./view/login/login.php" class="btn btn-primary col-sm-6" style="margin-top:250px; width:240px; height:45px; font-size:18px; padding-top:10px;">LOGIN</a>
-                <a href="./view/register/register.php" class="btn btn-primary col-sm-6" style="margin-top:250px; width:240px; height:45px; font-size:18px;margin-left:20px;padding-top:10px;">REGISTER</a>
+                <a href="./view/login/login.php" class="btn btn-primary col-sm-6"
+                    style="margin-top:250px; width:240px; height:45px; font-size:18px; padding-top:10px;">LOGIN</a>
+                <a href="./view/register/register.php" class="btn btn-primary col-sm-6"
+                    style="margin-top:250px; width:240px; height:45px; font-size:18px;margin-left:20px;padding-top:10px;">REGISTER</a>
             </center>
         </div>
     </div>
 </body>
+
 </html>
